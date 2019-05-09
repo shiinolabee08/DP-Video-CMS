1fe682fad0c5fabb49487bc07aa3a8283e5fc20e
AldrinP <aldrin.pernito@gmail.com>
2019-05-09 18:09:06 +0100

    Add the initial app with Basic Laravel Auth scaffolding, updates on Routes

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Resource for Employees
+Route::resource('employees', 'EmployeeController');
+
+//Resource for Companies
+Route::resource('companies', 'CompanyController');
+
+//Show images/assets from storage
+Route::get('storage/{filename}', function ($filename)
+{
+    $path = storage_path('app/public/logos/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
