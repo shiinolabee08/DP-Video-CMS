f467f59c348715adc4e39eb0b49055eae1ecda80
AldrinP <aldrin.pernito@gmail.com>
2019-06-12 19:16:57 +0100

    Added columns for form submissions table and some responsive bits

--- database/migrations/2019_06_06_132320_create_form_submissions_table.php
+++ database/migrations/2019_06_06_132320_create_form_submissions_table.php
@@ -15,6 +15,13 @@ class CreateFormSubmissionsTable extends Migration
     {
         Schema::create('form_submissions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('email')->unique()->nullable(false);
+            $table->string('phone_number')->nullable(true);
+            $table->mediumText('more_info');
+            $table->unsignedInteger('contact_form_id');
+
+            $table->foreign('contact_form_id')->references('id')->on('contact_forms');
             $table->timestamps();
         });
     }
--- resources/views/layouts/partials/header-nav.blade.php
+++ resources/views/layouts/partials/header-nav.blade.php
@@ -2,70 +2,72 @@
     
     <nav class="navbar navbar-expand-md fixed-top navbar-light shadow-sm">
         <div class="container">
-            <div class="col-md-2 col-sm-12">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ URL::to('/') }}/dplogo-light.svg" width="74" />
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
-
-            <div class="col-md-7 col-sm-12">
-                <div class="phone-top">
-                    <a href="tel:01702 749662"><i class="fas fa-phone"></i> 01702 749662</a>
+            <!-- <div  class="row"> -->
+                <div class="col-md-2 col-sm-12">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="{{ URL::to('/') }}/dplogo-light.svg" width="74" />
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <!-- <span class="navbar-toggler-icon"></span> -->
+                        <i class="fa fa-bars"></i> Menu
+                    </button>
                 </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav nav ml-md-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="{{ url('/services') }}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              {{ __('Services') }}
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('/services/corporate-video') }}">Corporate Video</a>
-                                <a class="dropdown-item" href="{{ url('/services/event-filming') }}">Event Filming</a>
-                                <a class="dropdown-item" href="{{ url('/services/logo-animation') }}">Logo Animation</a>
-                                <a class="dropdown-item" href="{{ url('/services/explainer-video') }}">Explainer Video</a>
-                                <a class="dropdown-item" href="{{ url('/services/facebook-cover-videos') }}">Facebook Cover Videos</a>
-                                <a class="dropdown-item" href="{{ url('/services/motion-graphics') }}">Motion Graphics</a>
-                                <a class="dropdown-item" href="{{ url('/services/promotional-video') }}">Promotional Video</a>
-                                <a class="dropdown-item" href="{{ url('/services/testimonial-video') }}">Testimonial Video</a>
-                                <a class="dropdown-item" href="{{ url('/services/youtube-editing') }}">Youtube Editing</a>
-                                <a class="dropdown-item" href="{{ url('/services/video-editing') }}">Video Editing</a>
-                                <a class="dropdown-item" href="{{ url('/services/wedding-video') }}">Wedding Video</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/our-work') }}">{{ __('Our Work') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/case-studies') }}">{{ __('Case Studies') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/testimonials') }}">{{ __('Testimonials') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/about-us') }}">{{ __('About') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/contact') }}">{{ __('Contact') }}</a>
-                        </li>          
-                    </ul>
+                <div class="col-md-7 col-sm-12">
+                    <div class="phone-top">
+                        <a href="tel:01702 749662"><i class="fas fa-phone"></i> 01702 749662</a>
+                    </div>
 
-                    <!-- Right Side Of Navbar -->
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-12">
-                <div class="free-quote">
-                    <a href="{{ url('/contact')}}" class="call-to-action">Get A Free Quote</a>
-                </div>
-            </div>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav nav ml-md-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="{{ url('/services') }}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  {{ __('Services') }}
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/services/corporate-video') }}">Corporate Video</a>
+                                    <a class="dropdown-item" href="{{ url('/services/event-filming') }}">Event Filming</a>
+                                    <a class="dropdown-item" href="{{ url('/services/logo-animation') }}">Logo Animation</a>
+                                    <a class="dropdown-item" href="{{ url('/services/explainer-video') }}">Explainer Video</a>
+                                    <a class="dropdown-item" href="{{ url('/services/facebook-cover-videos') }}">Facebook Cover Videos</a>
+                                    <a class="dropdown-item" href="{{ url('/services/motion-graphics') }}">Motion Graphics</a>
+                                    <a class="dropdown-item" href="{{ url('/services/promotional-video') }}">Promotional Video</a>
+                                    <a class="dropdown-item" href="{{ url('/services/testimonial-video') }}">Testimonial Video</a>
+                                    <a class="dropdown-item" href="{{ url('/services/youtube-editing') }}">Youtube Editing</a>
+                                    <a class="dropdown-item" href="{{ url('/services/video-editing') }}">Video Editing</a>
+                                    <a class="dropdown-item" href="{{ url('/services/wedding-video') }}">Wedding Video</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/our-work') }}">{{ __('Our Work') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/case-studies') }}">{{ __('Case Studies') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/testimonials') }}">{{ __('Testimonials') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/about-us') }}">{{ __('About') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/contact') }}">{{ __('Contact') }}</a>
+                            </li>          
+                        </ul>
 
+                        <!-- Right Side Of Navbar -->
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-12">
+                    <div class="free-quote">
+                        <a href="{{ url('/contact')}}" class="call-to-action">Get A Free Quote</a>
+                    </div>
+                </div>
+            <!-- </div> -->
         </div>
     </nav>
 </header>
